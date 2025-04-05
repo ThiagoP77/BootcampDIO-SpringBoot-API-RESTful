79d4c2018132cb14235ac8e0cd8b24029326be22
Thiago Piffer Lauro <thiagopifferlauro@gmail.com>
2025-04-05 16:14:44 -0300

    Ajuste Cors

--- src/main/java/me/dio/santander_dev_week_2023/SantanderDevWeek2023Application.java
+++ src/main/java/me/dio/santander_dev_week_2023/SantanderDevWeek2023Application.java
@@ -3,6 +3,12 @@ package me.dio.santander_dev_week_2023;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {
+	@Server(url = "/", description = "Default server url")
+})
 @SpringBootApplication
 public class SantanderDevWeek2023Application {
 
